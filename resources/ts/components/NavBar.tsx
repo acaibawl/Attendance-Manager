8380ebe1d89fd490f6814ca58508544b7ba9f3c9
acaiibawl <hagiya0719@gmail.com>
2022-01-24 16:58:55 +0900

    mui 導入

--- resources/ts/components/NavBar.tsx
+++ resources/ts/components/NavBar.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import HomeIcon from '@mui/icons-material/Home';
 
 const NavBar = () => {
     return (
         <div>
-            <Link to="/">Home</Link>
+            <Link to="/"><HomeIcon /> Home</Link>
             <Link to="/about">About</Link>
             <Link to="/tasks">tasks</Link>
             <Link to="/projects">projects</Link>
